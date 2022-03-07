3859b2e1662ec86f09e64eda1cb8ebf7c4d9fe7c
Kevin V <95386053+kevinvo711@users.noreply.github.com>
2022-03-07 13:06:50 -0500

    added html parser

--- search.py
+++ search.py
@@ -1,3 +1,36 @@
 """
+dependencies:
+scikit-learn
+pandas
+beautifulsoup4
+lxml
+"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import pandas as pd
+from bs4 import BeautifulSoup
+import os
+
 
 """
+Uses BeautifulSoup package to parse HTML, using 'lxml' parser
+
+
+@param myFile
+location of file as string
+
+@returns "string" as data in HTML page
+"""
+def parseHTML(myFile):
+    with open(myFile, 'r') as fp:
+        soup = BeautifulSoup(fp, features='lxml') 
+        # print(fp.read())
+        # fp.seek(0)
+        return soup.get_text()
+
+def main():
+    directoryName = "./data/wikipedia-trimmed"
+    list = os.listdir(directoryName)
+    
+
+main()
