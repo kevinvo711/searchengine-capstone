a7b6c83509b0a159152c53c293a01e3afa7b5d8f
Kevin V <95386053+kevinvo711@users.noreply.github.com>
2022-03-21 06:28:18 -0400

    Store for later

--- search.py
+++ search.py
@@ -14,43 +14,57 @@ from bs4 import BeautifulSoup
 import os
 
 
+"""
+Dirty implementation of a set of test HTML files for running TF-IDF on.
+Ideally can run on any HTML page, and produce a {key:value} dictionary of every 'word' with 'tf-idf' value
+Ran on a test set of 50 files successfully
 
+"""
 
 def main():
     directoryName = "./data/wikipedia-trimmed"
     # testDirectory = "./testfolder"
     vectorizer = TfidfVectorizer(stop_words="english") #vectorizer object of TfidfVectorizer class
-    # X = vectorizer.fit_transform()
 
     myDictionaryTuple = {}
     dictionaryList = []
-    with os.scandir(directoryName) as allMyFiles:
+    with os.scandir(directoryName) as allMyFiles: #opens the directory folder and iterates through every file (expected input is html page)
         for eachFile in allMyFiles:
             matrixObj = vectorizer.fit_transform(parseHTML(eachFile)) #vectorizer needs to transform on a dictionary/list - type object
-            tokens = vectorizer.get_feature_names_out()
-            tokenList = []
+            tokens = vectorizer.get_feature_names_out() #the list of words'
+
+            """
             # myDictionaryTuple = dict(zip(tokenList, [matrixObj.toarray()))
             # myDictionaryTuple = {key:value for key, value in zip(tokens,matrixObj.toarray())}
-            cooMatrix = matrixObj.tocoo()
+            """
+
+            cooMatrix = matrixObj.tocoo() #convert scipy.sparse matrix to coordinate format matrix, to able iterate through the data values
+           
+            """
             # for i, j, v in zip(cooMatrix.row, cooMatrix.col, cooMatrix.data):
             #     print ("(%d, %d), %s" % (i, j, v))
             # cooMatrix = scipy.sparse.coo_matrix(matrixObj)
+            """
             myDictionaryTuple = dict(zip(tokens,cooMatrix.data))
-            dictionaryList.append(myDictionaryTuple) 
-"""
-Have dictionary of stuffs, now to put it into Postgresql
-"""
+            dictionaryList.append(myDictionaryTuple)
 
-    # myDf = pd.DataFrame(data=matrixObj.toarray(), columns=tokens)
-    # print(len(myDictionaryTuple))
-    # print(myDictionaryTuple)
-    # print(matrixObj)
-    # print(myDf.head(30)) 
-    # transposed = myDf.transpose() #2 
-    # print(transposed.head(30)) #1
-    # transposed = transposed.sort_index(axis = 1, ascending = False)
-    # print(transposed.head(30))
+    """Have dictionary of stuffs, now to put it into Postgresql"""
+
+    print("hello")
+    myDf = pd.DataFrame(data=matrixObj.toarray(), columns=tokens)
+
+
+    print(len(myDictionaryTuple))
+    print(myDictionaryTuple)
+    print(matrixObj)
+    print(myDf.head(30))
+    transposed = myDf.transpose()  # 2
+    print(transposed.head(30))  # 1
+    # transposed = transposed.sort_index(axis=1, ascending=False)
+    print(transposed.head(30))
     print(len(dictionaryList))
+
+
 
 
 """
