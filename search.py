fe956eac9f1155be9ed7b71937e7836ef759a8b6
StephonKumar2000 <56920459+StephonKumar2000@users.noreply.github.com>
2022-03-28 11:38:36 -0400

    Adding Indexing
    Its not completed yet I'm still working on it.

--- search.py
+++ search.py
@@ -12,27 +12,88 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 import os
-
-def main():
-    directoryName = "./data/wikipedia-trimmed" #test directory folder
-    vectorizer = TfidfVectorizer(max_df=.65, min_df=1, stop_words="english", use_idf=True, norm=None) #vectorize object
-
-    with os.scandir(directoryName) as allFiles: #opens directory folder & looks at each file
-        for allDocs in allFiles:
-            transDocs = vectorizer.fit_transform(allDocs)#converts the list of strings to a sparse matrix
-            transDocs_as_array = transDocs.toarray() #making sure the file list has the same number of documents in the array
-
-            len(transDocs_as_array)
-
+import termlist
 
 
 """
+Dirty implementation of a set of test HTML files for running TF-IDF on.
+Ideally can run on any HTML page, and produce a {key:value} dictionary of every 'word' with 'tf-idf' value
+Ran on a test set of 50 files successfully
+"""
+
+def main():
+    directoryName = "./data/wikipedia-trimmed"
+    # testDirectory = "./testfolder"
+    vectorizer = TfidfVectorizer(stop_words="english") #vectorizer object of TfidfVectorizer class
+
+    myDictionaryTuple = {}
+    dictionaryList = []
+    with os.scandir(directoryName) as allMyFiles: #opens the directory folder and iterates through every file (expected input is html page)
+        for eachFile in allMyFiles:
+            matrixObj = vectorizer.fit_transform(parseHTML(eachFile)) #vectorizer needs to transform on a dictionary/list - type object
+            tokens = vectorizer.get_feature_names_out() #the list of words'
+
+            """
+            # myDictionaryTuple = dict(zip(tokenList, [matrixObj.toarray()))
+            # myDictionaryTuple = {key:value for key, value in zip(tokens,matrixObj.toarray())}
+            """
+
+            cooMatrix = matrixObj.tocoo() #convert scipy.sparse matrix to coordinate format matrix, to able iterate through the data values
+
+            """
+            # for i, j, v in zip(cooMatrix.row, cooMatrix.col, cooMatrix.data):
+            #     print ("(%d, %d), %s" % (i, j, v))
+            # cooMatrix = scipy.sparse.coo_matrix(matrixObj)
+            """
+            myDictionaryTuple = dict(zip(tokens,cooMatrix.data))
+            dictionaryList.append(myDictionaryTuple)
+
+    """Have dictionary of stuffs, now to put it into Postgresql"""
+
+    myDf = pd.DataFrame(data=matrixObj.toarray(), columns=tokens) #just for formatting and to look inside
+
+    print(myDictionaryTuple)
+    print(myDf.head(30))
+    transposed = myDf.transpose()  # 2
+    print(transposed.head(30))  # 1
+    # transposed = transposed.sort_index(axis=1, ascending=False)
+    print(transposed.head(30))
+    print(len(dictionaryList)) #run on list of html pages, should equal to number of files in directory, in this case, 50
+
+""" Indexing the search """
+#*** Not completed ***
+def index_one_file(termlist):
+	fileIndex = {}
+	for index, word in enumerate(termlist):
+		if word in fileIndex.keys():
+			fileIndex[word].append(index)
+		else:
+			fileIndex[word] = [index]
+	return fileIndex
+
+    def make_indices(termlists):
+	total = {}
+	for directoryName in termlists.keys():
+		total[directoryName] = index_one_file(termlists[directoryName])
+	return total
+
+def fullIndex(regdex):
+	total_index = {}
+	for directoryName in regdex.keys():
+		for word in regdex[directoryName].keys():
+			if word in total_index.keys():
+				if directoryName in total_index[word].keys():
+					total_index[word][directoryName].extend(regdex[directoryName][word][:])
+				else:
+					total_index[word][directoryName] = regdex[directoryName][word]
+			else:
+				total_index[word] = {directoryName: regdex[directoryName][word]}
+	return total_index
+#*** Not completed ***
+"""
 Uses BeautifulSoup package to parse HTML, using 'lxml' parser
-
-
 @param fileName
 location of file as string
-
 @returns data in HTML page as list
 """
 
